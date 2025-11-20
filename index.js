4be24c4de15bd1dda96870902bbd090d2f0a1b3e
Aldo Valente <34844225+aldvalente@users.noreply.github.com>
2025-11-20 20:34:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-  res.end("ValentePro Apps - Dokku 🚀");
+  res.end("ValentePro Apps ");
 });
 
 server.listen(port);
