50d6d27b1c1361bcf5c13ce41971a7bbb0c4cda9
Aldo Valente <34844225+aldvalente@users.noreply.github.com>
2025-11-21 15:25:04 +0000

    Eliminazione

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const port = process.env.PORT || 5000;
-
-const server = http.createServer((req, res) => {
-  res.end("ValentePro Apps - Git App 2");
-});
-
-server.listen(port);
-console.log("Server in ascolto su " + port);
