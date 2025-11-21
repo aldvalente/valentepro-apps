80c665487e82dbab4f2bc53e9675a66e9b7aabb4
Aldo Valente <34844225+aldvalente@users.noreply.github.com>
2025-11-21 09:37:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-  res.end("ValentePro Apps ");
+  res.end("ValentePro Apps - Finalmente riesco a deployare ");
 });
 
 server.listen(port);
