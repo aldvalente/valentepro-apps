71c51fd19519334b81f286e42d518396519dcfad
Aldo Valente <34844225+aldvalente@users.noreply.github.com>
2025-11-23 19:59:54 +0100

    Fix typo in server response message

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("ValentePro running!"));
+app.get("/", (req, res) => res.send("ValentePro running!s"));
 app.listen(process.env.PORT || 5000);
