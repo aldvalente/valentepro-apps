f46b3d1e7e945c2ce9974fd12223de115f6a9db6
Aldo Valente <34844225+aldvalente@users.noreply.github.com>
2025-11-23 20:09:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("ValentePro running!s"));
+app.get("/", (req, res) => res.send("ValentePro running!"));
 app.listen(process.env.PORT || 5000);
